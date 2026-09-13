c615fa440a4a8010d23e55ed9762172c2f56f1d7
SaiNandhan06 <sainandhanmucharla@gmail.com>
2026-09-13 12:15:00 +0530

    feat(auth-service): add register/login DTOs and auth response shape

---
+++ authService/src/main/java/resolvenow/dto/RegisterRequest.java
@@ -0,0 +1,16 @@
+package resolvenow.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotBlank
+    private String username;
+
+    @Email @NotBlank
+    private String email;
+
+    @NotBlank @Size(min = 8)
+    private String password;
+}
