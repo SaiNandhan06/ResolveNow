ab05d4a3ab7b45b416f6ac1226967efcac7f1c26
SaiNandhan06 <sainandhanmucharla@gmail.com>
2026-09-14 00:25:08 +0530

    chore: add service scaffolding, launchers, servlet initializers, and tests

---
+++ apiGateway/src/test/java/resolvenow/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package resolvenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ complaintService/src/test/java/resolvenow/ComplaintServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package resolvenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
